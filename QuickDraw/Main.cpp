7acae342150ce9cb9ea81aae9b6d7d6613ab25d9
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2018-09-12 16:47:15 +0100

    Added a timer

--- QuickDraw/Main.cpp
+++ QuickDraw/Main.cpp
@@ -1,17 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 int main()
 {
 	sf::RenderWindow gameWindow(sf::VideoMode::getDesktopMode(), "Quick Draw",
 	sf::Style::Titlebar | sf::Style::Close);
 
-
+	//seed our random number generator
+	srand(time(NULL));
 
 	//timer functionality
 	float signalTimeLowerLimit = 5.0f;
 	float signalTimeUpperLimit = 10.0f;
-	sf::Time timeTilSignal= sf::seconds(0.0f); 
+	sf::Time timeTilSignal= sf::seconds(signalTimeLowerLimit); 
 	sf::Time timeSinceSignal = sf::seconds(0.0f);
 	sf::Clock gameClock;
 
@@ -30,15 +33,38 @@ int main()
 		gameWindow.getSize().y / 2 - buttonTexture.getSize().y / 2
 	);
 
-
+	
 	while (gameWindow.isOpen())
 	{
+		sf::Event gameEvent;
 		sf::Event event;
-		while (gameWindow.pollEvent(event))
+		while (gameWindow.pollEvent(gameEvent))
 		{
-			if (event.type == sf::Event::Closed)
+			if (gameEvent.type == sf::Event::MouseButtonPressed)
+			{
+				if (buttonSprite.getGlobalBounds().contains(gameEvent.mouseButton.x, gameEvent.mouseButton.y))
+				{
+					//we did
+					int range = (int)(signalTimeUpperLimit - signalTimeLowerLimit);
+					float signalSeconds = rand() % range + signalTimeLowerLimit;
+					timeTilSignal - sf::seconds(signalSeconds);
+				}
+			}
+			if (gameEvent.type == sf::Event::Closed)
 				gameWindow.close();
 		}
+
+		//update game state
+		sf::Time frameTime = gameClock.restart();
+
+		timeTilSignal = timeTilSignal - frameTime;
+		if (timeTilSignal.asSeconds() <= 0.0f)
+		{
+			//signal is go
+			buttonSprite.setColor(sf::Color::Red);
+		}
+
+
 		gameWindow.clear(sf::Color::Red);
 		gameWindow.draw(buttonSprite);
 		gameWindow.display();
